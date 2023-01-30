1afbf46700c532f5752bd1f2efe0ede2cdd8f8a9
martin <labram03@gmail.com>
2023-01-29 22:36:54 -0500

    first commit

---
+++ src/main/java/com/example/HWJenkins/HwJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.HWJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HWJenkins/HwJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HWJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
